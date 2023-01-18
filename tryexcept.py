74266ab10cafe0d084f072a23ebbb0025006ae08
WendyKariuki <phaminuswendy@gmail.com>
2023-01-18 14:11:53 +0300

    zero division error

--- tryexcept.py
+++ tryexcept.py
@@ -15,3 +15,12 @@ except:
     istr = -1
 finally:
     print("the finally block runs regardless of any error")
+
+a = 10
+b = 0
+try:
+    print(a/b)
+except:
+    print("You cannot divide a number by zero")
+finally:
+    print("The end")
