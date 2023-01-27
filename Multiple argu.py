45a4a1fea5fd16527299dc27a947e039c4faa1ef
WendyKariuki <phaminuswendy@gmail.com>
2023-01-27 09:00:14 +0300

    multiple arguments

--- Multiple argu.py
+++ Multiple argu.py
@@ -1,1 +1,2 @@
-print("I'm Wendy","I'm beautiful", "and i love dogs", sep=" ", end="."
+print("I'm Wendy","I'm beautiful", "and i love dogs", sep=" ", end=".")
+print("I'm Wendy", "and i'm learning python", sep=" ", end=".")
