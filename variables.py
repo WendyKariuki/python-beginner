f6f81cffd8429a24886bee777b4bc022adcdc42c
WendyKariuki <phaminuswendy@gmail.com>
2023-02-09 06:50:53 +0300

    Variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+var = '1'
+var2 = "1"
+print(var + var2)
+
+a = 6
+b = 3
+a /= 2 * b
+print(a)
+
+# a and b are the lengths of the legs
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5 # We use ** instead of squareroot.
+print("c =", c) 
+
