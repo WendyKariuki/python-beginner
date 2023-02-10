c988e50f4b6343534e84d58b1b852567dbe50bfd
WendyKariuki <phaminuswendy@gmail.com>
2023-02-10 07:05:26 +0300

    Input fn

---
+++ input fn.py
@@ -0,0 +1,11 @@
+anything = float(input("Enter a number: "))
+something = anything ** 2.0
+print(anything, "to the power of 2.0 is",  something)
+
+fnam = input("May I have your first name, please? ")
+lnam = input("May I have your last name, please? ")
+print("Thank you.")
+print("\nYour name is " + fnam + " " + lnam + ".")
+
+
+
