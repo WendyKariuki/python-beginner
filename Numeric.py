df73316505c40f5f9d2986175e2616ba05cd5301
WendyKariuki <phaminuswendy@gmail.com>
2023-02-20 08:35:26 +0300

    replicator

---
+++ Numeric.py
@@ -0,0 +1,67 @@
+x = 2 + 1
+print(x)
+x = 4 
+y = 2
+print(x)
+if x > y:
+    print("x is greater than y")
+else:
+    print("x is less than y")
+
+x = 42
+if x > 1 :
+    print("Greater than one")
+    if x < 100 :
+        print("Less than 100")
+
+x = 4
+if x > 2 :
+    print("bigger")
+else :
+    print("smaller")
+
+x = 0
+if x < 2 :
+    print("Small")
+elif x < 10 :
+    print("Medium")
+else :
+    print("Large")
+
+x = 20
+if x < 2 :
+    print("Small")
+elif x < 10 :
+    print("Medium")
+else :
+    print("Large")
+
+x = 5
+if x < 2 :
+    print("Small")
+elif x < 10 :
+    print("Medium")
+
+x = 20
+y = 43
+if x > y :
+    print("x is greater than y")
+elif x == y :
+    print("x ix equal to y")
+elif x < y :
+    print("x is less than y")
+else :
+    print("x is equal to zero")
+
+print(6 // 4)
+print(6. //4)
+print(6 / 4)
+print(6. / 4)
+print(14 % 7)
+print(12 % 4.5)
+
+var = "3.8.5"
+print("python version:" + var)
+var = 100
+var = 200+300
+print(var)
