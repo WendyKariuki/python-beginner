cb485292d3ccce14593bf1ee604860a5c5f204bc
WendyKariuki <phaminuswendy@gmail.com>
2023-03-06 13:16:59 +0300

    comparison

---
+++ comparison.py
@@ -0,0 +1,19 @@
+n = int(input("Enter a number: "))
+if n <= 100:
+    print(False)
+else:
+    print(True)
+
+n = int(input("Enter a number: "))
+print(n <= 100)
+
+plant = input("Enter a plant: ")
+if plant == "Spathiphyllum":
+    print("Yes- Spathiphyllum is the best plant ever!")
+elif plant == "spathiphyllum":
+    print("No, i want a big Spathiphyllum!")
+else:
+    print("Spathiphyllum! not palergonium")
+    
+
+
