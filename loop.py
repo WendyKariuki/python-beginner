fa9aa23ad68dca928492dad02a77e7fdb6f718b1
WendyKariuki <phaminuswendy@gmail.com>
2023-03-07 07:57:34 +0300

    loop

---
+++ loop.py
@@ -0,0 +1,16 @@
+secretnumber = 777
+number = int(input("Guess a number:" ))
+
+while number != secretnumber:
+    print("Haha! You're stuck in my loop!")
+    number = int(input("Guess a number:"))
+   
+print( """
++================================+
+| Welcome to my game, muggle!    |
+| Enter an integer number        |
+| and guess what number I've     |
+| picked for you.                |
+| So, what is the secret number? |
++================================+
+""")
