46d587639a9aef6a4fd9be0d97db77be65cfe118
WendyKariuki <phaminuswendy@gmail.com>
2023-03-08 07:58:46 +0300

    Forloop

---
+++ forloop.py
@@ -0,0 +1,22 @@
+for i in range(10):
+    print("The value of i is currently", i)
+
+for i in range(2, 8):
+    print("The value of i is currently", i)
+
+for i in range(2, 8, 3):
+    print("The value of i is currently", i)
+
+for i in range(1, 1):
+    print("The value of i is currently", i)
+
+power = 1
+for expo in range(16):
+    print("2 to the power of", expo, "is", power)
+    power *= 2
+
+import time
+for mississipi in range(1, 5):
+    print("The value of mississipi is currently", mississipi)
+print("Ready or not, here I come!")
+
